6cecaf3054ea115849a38359fc013729c99e00ca
Arpit-web <arpitwebdev188@gmail.com>
2024-09-24 17:59:58 +0530

    fifth commit

--- backend/database/dbConnection.js
+++ backend/database/dbConnection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const dbConnection = () => {
   mongoose
-    .connect("process.env.MONGO_URI", { dbName: "Client Messages" })
+    .connect(process.env.MONGO_URI , { dbName: "EVENTIQUE" })
     .then(() => {
       console.log("Connected to database!");
     })
